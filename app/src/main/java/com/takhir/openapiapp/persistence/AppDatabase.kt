e35a176940c8ddd08b143da0069a2c07a59e2685
takhir-nafikov <takhir.learn@gmail.com>
2020-11-08 20:13:41 +0300

    Room persistence setup

--- app/src/main/java/com/takhir/openapiapp/persistence/AppDatabase.kt
+++ app/src/main/java/com/takhir/openapiapp/persistence/AppDatabase.kt
@@ -1,4 +1,20 @@
 package com.takhir.openapiapp.persistence
 
-abstract class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.takhir.openapiapp.models.AccountProperties
+import com.takhir.openapiapp.models.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDatabase: RoomDatabase {
+
+    companion object {
+
+        const val DATABASE_NAME = "app_db"
+    }
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
 }
