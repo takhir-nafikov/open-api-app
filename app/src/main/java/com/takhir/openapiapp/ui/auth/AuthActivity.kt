66b3c1fdc65afd9effb41ee3b5e13795cb7c0f3b
takhir-nafikov <takhir.learn@gmail.com>
2020-11-08 18:30:54 +0300

    added auth nav graph

--- app/src/main/java/com/takhir/openapiapp/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/takhir/openapiapp/ui/auth/AuthActivity.kt
@@ -1,7 +1,8 @@
-package com.takhir.openapiapp
+package com.takhir.openapiapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.takhir.openapiapp.R
 
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
