1b8acaaaf6eb4af781907f845719eb0c29b17711
takhir-nafikov <takhir.learn@gmail.com>
2021-02-21 14:15:01 +0300

    Fix view binding library bug

--- app/src/main/java/com/takhir/openapiapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/takhir/openapiapp/ui/main/MainActivity.kt
@@ -4,18 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.takhir.openapiapp.databinding.ActivityMainBinding
 import com.takhir.openapiapp.ui.BaseActivity
 import com.takhir.openapiapp.ui.auth.AuthActivity
 
 class MainActivity : BaseActivity() {
 
-  // TODO не работет либа для активити, отписать автору или сам неверно исопльзую ?
-  private lateinit var  binding: ActivityMainBinding
+  private val  binding: ActivityMainBinding by viewBinding(CreateMethod.INFLATE)
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
     binding.toolBar.setOnClickListener {
