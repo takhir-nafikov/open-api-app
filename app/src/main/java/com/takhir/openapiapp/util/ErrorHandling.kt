d1b8c393b1c45e6d85096c9953cca3d348aed8e1
takhir-nafikov <takhir.learn@gmail.com>
2021-02-27 23:15:30 +0300

    NetworkBoundResource

--- app/src/main/java/com/takhir/openapiapp/util/ErrorHandling.kt
+++ app/src/main/java/com/takhir/openapiapp/util/ErrorHandling.kt
@@ -4,5 +4,16 @@ class ErrorHandling {
 
   companion object {
     const val  ERROR_UNKNOWN = "Unknown Error."
+
+    const val UNABLE_TO_RESOLVE_HOST = "Unable to resolve host"
+    const val UNABLE_TODO_OPERATION_WO_INTERNET = "Can't do that operation without an internet connection"
+    const val ERROR_CHECK_NETWORK_CONNECTION = "Check network connection."
+
+    fun isNetworkError(msg: String): Boolean {
+      return when {
+        msg.contains(UNABLE_TO_RESOLVE_HOST) -> true
+        else -> false
+      }
+    }
   }
 }
